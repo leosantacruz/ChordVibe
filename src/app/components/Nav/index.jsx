2181e90736e9f7b158db5ca4a2d64a55277660b4
Leonardo Santa Cruz <leosc86@gmail.com>
2025-03-27 22:44:57 -0300

    Add GitHub repo

--- src/app/components/Nav/index.jsx
+++ src/app/components/Nav/index.jsx
@@ -7,12 +7,18 @@ const Nav = () => {
     <nav className="fixed z-50 w-full text-white top-0 ">
       <div className="flex p-4 backdrop-blur-md items-center justify-between">
         <div className="flex items-center gap-10">
-          <Image src="/images/logo.webp" width={150} height={"100"}></Image>
+          <Link href="/">
+            <Image src="/images/logo.webp" width={150} height={"100"}></Image>
+          </Link>
           <Link href="/">Play</Link>
           <Link href="/collection">Collection</Link>
         </div>
-        <Link href="" className="flex items-center gap-3">
-          <Github /> See on Github
+        <Link
+          href="https://github.com/leosantacruz/ChordVibe"
+          target="_blank"
+          className="flex items-center gap-3"
+        >
+          <Github /> See on GitHub
         </Link>
       </div>
       <div className="bg-white opacity-10 h-[1px]"></div>
